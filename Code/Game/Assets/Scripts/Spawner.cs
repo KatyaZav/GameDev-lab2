96075ea9abf6d65bd2887864ef0cf04de8560299
KatyaZav <party2015ru@mail.ru>
2022-10-04 20:30:52 +0500

    random moving for spawner

---
+++ Code/Game/Assets/Scripts/Spawner.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] float MaxSpeed;
+    [SerializeField] float movementDirection;
+
+    [SerializeField] float speed;
+    Rigidbody2D _rb;
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        speed = MaxSpeed;
+
+        StartCoroutine(Moving());
+    }
+
+    private IEnumerator Moving()
+    {
+        while (true)
+        {
+            yield return new WaitForFixedUpdate();
+
+            transform.Translate((new Vector2(movementDirection, 0)) * MaxSpeed * Time.fixedDeltaTime);
+
+            if (Random.Range(1, 100) < 4)
+            {
+                movementDirection *= -1;
+            }
+           
+            if (Random.Range(0, 10) > 5)
+            {
+                speed = Random.Range(0, MaxSpeed);
+            }
+        }
+    }
+}
